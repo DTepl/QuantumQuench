0a519f7b0d884c0fd5fa6e23e02820db804b08c8
DTepl <daniiltepl@gmail.com>
2023-11-05 16:06:52 +0100

    Added Evolution circuit and script

---
+++ src/IsingEvolution.py
@@ -0,0 +1,41 @@
+import pennylane as qml
+import matplotlib.pyplot as plt
+
+
+class IsingEvol():
+
+    def __init__(self, N, dt, h, J):
+        self.N = N
+        self.dt = dt
+        self.h = h
+        self.J = J
+        self.dev = qml.device("lightning.qubit", wires=range(N))
+
+    def evolution_step(self):
+        for idx in range(self.N):
+            qml.RZ(2 * self.h * self.dt, wires=idx)
+
+        for idx in range(self.N - 1):
+            qml.IsingXX(2 * self.J * self.dt, wires=[idx, idx + 1])
+
+    def quench_process(self, obs_Z=[0], obs_XX=[], steps=1):
+        for idx in range(self.N):
+            qml.Hadamard(idx)
+
+        for _ in range(steps):
+            self.evolution_step()
+        return [qml.expval(qml.PauliZ(i)) for i in obs_Z] + [
+            qml.expval(qml.PauliX(i[0]) @ qml.PauliX(i[1])) for i in obs_XX]
+
+    def circuit(self):
+        return qml.QNode(self.quench_process, self.dev)
+
+    def plot(self, data, obs_Z, obs_XX):
+        for i in range(len(obs_Z)):
+            plt.plot(data[:, i], label=r'$\langle \sigma^z_{{}} \rangle $'.format(obs_Z[i]))
+        for i in range(len(obs_XX)):
+            plt.plot(data[:, len(obs_Z) + i],
+                     label=r'$\langle \sigma^X_{{{}}} \sigma^X_{{{}}} \rangle$'.format(obs_XX[i][0], obs_XX[i][1]))
+        plt.xlabel('trotter steps')
+        plt.legend()
+        plt.savefig('quench.png')
---
+++ src/dynamics.py
@@ -0,0 +1,24 @@
+import cirq
+import numpy as np
+import tqdm
+import pennylane as qml
+
+from src.IsingEvolution import IsingEvol
+
+N = 16
+J = 1
+h = 0.3
+dt = 0.1  # time step
+obs_Z = [2, 15]
+obs_XX = [[1, 13], [5, 14]]
+
+ising_model_evolution = IsingEvol(N, dt, h, J)
+print(qml.draw(ising_model_evolution.circuit())())
+
+results = []
+for n in tqdm.tqdm(range(1, 200)):
+    results.append(ising_model_evolution.circuit()(obs_Z=obs_Z, obs_XX=obs_XX, steps=n))
+results = np.array(results)
+
+print(results.shape)
+ising_model_evolution.plot(results, obs_Z, obs_XX)
